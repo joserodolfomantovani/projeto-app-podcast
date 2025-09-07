9f2a6b291a6c156300e03b9b26b79ed58e44cb78
Rodolfo <j10rodolfo@gmail.com>
2025-09-07 18:42:29 -0300

    adiciona serviço e repositório para listar episódios de podcasts

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,29 +1,11 @@
 import { IncomingMessage, ServerResponse } from "http"
+import { serviceListEpisodes } from "../services/list-epsodes-services"
 
 export const getListEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
 
+    const content = await serviceListEpisodes()
     response.writeHead(200, { "Content-Type": "application/json" })
-    response.end(JSON.stringify(
-        [{
-            "podcastName": "flow",
-            "episode": "CBUM - Flow #319",
-            "videoId": "pQSuQmUfS30",
-            "cover": "https://i.ytimg.com/vi/pQSuQmUfS30/maxresdefault.jpg",
-            "Link": "https://www.youtube.com/watch?v=pQSuQmUfS30",
-            "categortes": ["saúde", "esporte", "bodybuilder"]
-        },
-        {
-            "podcastName": "flow",
-            "episode": "RUBENS BARRICHELLO - Flow #339",
-            "videoId": "4KDGTdioV4I",
-            "cover": "https://i.ytimg.com/vi/4KDGTdiOV4I/maxresdefault.jpg",
-            "link": "https://www.youtube.com/watch?v=4KDGTdiOV4I",
-            "categories": ["esporte", "corrida"]
-        }
-
-        ])
-
-    )
+    response.end(JSON.stringify(content))
 
 
 }
